1bb9d8199b9205b8ad75cbd07902352ee337b4e0
JsebastianDS <137601010+JsebastianUVPRQ@users.noreply.github.com>
2025-11-28 16:32:52 -0500

    Update app.py

--- calc_app/app.py
+++ calc_app/app.py
@@ -117,7 +117,7 @@ def main():
     
     country = st.selectbox('Currency', ['USD', 'COP'])
     year = st.select_slider('Year', options=list(range(1989, 2024)))
-    amount = st.number_input('Amount', min_value=0, value=0)
+    amount = st.number_input('Amount', min_value=0, value=1000, step=500)
     
     if st.button('Calculate'):
         result = calculate_value(year, country, amount)
